ed6b088040dfe5dde2bb4c2f86aad1664b11cf68
DESKTOP-GSTOGUR\MJ <m_veloper@naver.com>
2020-08-28 00:36:14 +0900

    no message

--- e_shop/user/form.py
+++ e_shop/user/form.py
@@ -69,4 +69,6 @@ class LoginForm(forms.Form):
 
             if not check_password(password, user.password):
                 self.add_error('password', '비밀번호를 틀렸습니다')
+            else:
+                self.email = user.email
 
