76423c1494bc79e9de701679800642d0504fe203
DESKTOP-GSTOGUR\MJ <m_veloper@naver.com>
2020-08-28 00:21:34 +0900

    no message

--- e_shop/user/form.py
+++ e_shop/user/form.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.hashers import check_password
 from .models import User
 
 
@@ -38,3 +39,34 @@ class RegisterForm(forms.Form):
                     password = password
                 )
                 user.save()
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(
+        error_messages={
+            'required': '이메일을 입력해주세요.'
+        },
+        max_length=64, label='이메일'
+    )
+    password = forms.CharField(
+        error_messages={
+            'required': '비밀번호를 입력해주세요.'
+        },
+        widget=forms.Textarea, label='비밀번호'
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
+
+        if email and password:
+            try:
+                user = User.objects.get(email=email)
+            except User.DoesNotExist:
+                self.add_error('email', '아이디가 없습니다')
+                return
+
+            if not check_password(password, user.password):
+                self.add_error('password', '비밀번호를 틀렸습니다')
+
