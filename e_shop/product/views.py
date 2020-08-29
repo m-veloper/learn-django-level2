9639196891f89cae1d84ea816ba67b4cf412601a
DESKTOP-GSTOGUR\MJ <m_veloper@naver.com>
2020-08-29 21:37:25 +0900

    no message

--- e_shop/product/views.py
+++ e_shop/product/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Product
 # Create your views here.
+
+class ProductList(ListView):
+    model = Product
+    template_name = 'product.html'
+    context_object_name = 'product_list'
