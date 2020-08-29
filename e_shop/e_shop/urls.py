c184a84ad5468146e380213e2e6921316925ac0d
DESKTOP-GSTOGUR\MJ <m_veloper@naver.com>
2020-08-29 21:37:14 +0900

    no message

--- e_shop/e_shop/urls.py
+++ e_shop/e_shop/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from user.views import index, RegisterView, LoginView
+from product.views import ProductList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('register/', RegisterView.as_view()),
-    path('login/', LoginView.as_view())
+    path('login/', LoginView.as_view()),
+    path('product/', ProductList.as_view())
 ]
