efa60286581fd54fcfae83e50b55d613a109e2fd
DESKTOP-GSTOGUR\MJ <m_veloper@naver.com>
2020-08-29 22:21:11 +0900

    no message

--- e_shop/e_shop/urls.py
+++ e_shop/e_shop/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from user.views import index, RegisterView, LoginView
-from product.views import ProductList
+from product.views import ProductList, ProductCreate
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
-    path('product/', ProductList.as_view())
+    path('product/', ProductList.as_view()),
+    path('product/create/', ProductCreate.as_view())
 ]
--- e_shop/product/views.py
+++ e_shop/product/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic.edit import FormView
 from .models import Product
+from .form import RegisterForm
+
+
 # Create your views here.
 
 class ProductList(ListView):
     model = Product
     template_name = 'product.html'
     context_object_name = 'product_list'
+
+class ProductCreate(FormView):
+    template_name = 'register_product.html'
+    form_class = RegisterForm
+    success_url = '/product/'
+
+
