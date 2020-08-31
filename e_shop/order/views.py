3e15dd9b28915c29e55cf6f6f7791969fcfced58
DESKTOP-GSTOGUR\MJ <m_veloper@naver.com>
2020-09-01 00:54:48 +0900

    no message

--- e_shop/order/views.py
+++ e_shop/order/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import FormView
+from .form import RegisterForm
 
 # Create your views here.
+
+class OrderCreate(FormView):
+    form_class = RegisterForm
+    success_url = '/product/'
+
+    def form_invalid(self, form):
+        return redirect('/product/' + str(form.product))
+
+    def get_form_kwargs(self, **kwargs):
+        kw = super(OrderCreate, self).get_form_kwargs(**kwargs)
+        kw.update({
+            'request': self.request
+        })
+        return kw
